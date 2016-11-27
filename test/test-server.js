4516955d5b434858ccab4d68b28127aadde9bbaa
shellbell22 <morris.shelley@gmail.com>
2016-11-27 14:36:27 -0500

    after adding travis file

---
+++ test/test-server.js
@@ -0,0 +1,30 @@
+/* example text
+describe('Shopping List', function() {
+    it('should list items on get');
+    it('should add an item on post');
+    it('should edit an item on put');
+    it('should delete an item on delete');
+});
+*/
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server.js');
+
+var should = chai.should();
+var app = server.app;
+var storage = server.storage;
+
+chai.use(chaiHttp);
+
+describe('index page', function() {
+  it('exists', function(done) {
+    chai.request(app)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+    });
+  });
+});
