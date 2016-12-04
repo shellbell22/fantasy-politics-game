9443e5797cd8d1c5691236ac828a8164e0838cf0
shellbell22 <morris.shelley@gmail.com>
2016-12-04 17:02:12 -0500

    mock data printed

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 var SENATOR_LIST = {
     "senators": [{
             "personid": 412582,
-            "bioguideid": K000384,
+            "bioguideid": "K000384",
             "firstname": "Timothy",
             "lastname": "Kaine",
             "image": "https://www.govtrack.us/data/photos/412582-200px.jpeg",
@@ -16,7 +16,7 @@ var SENATOR_LIST = {
 
         {
             "personid": 412573,
-            "bioguideid": C001098,
+            "bioguideid": "C001098",
             "firstname": "Ted",
             "lastname": "Cruz",
             "image": "https://www.govtrack.us/data/photos/412573-200px.jpeg",
@@ -31,7 +31,7 @@ var SENATOR_LIST = {
 
         {
             "personid": 412556,
-            "bioguideid": F000463,
+            "bioguideid": "F000463",
             "firstname": "Deb",
             "lastname": "Fischer",
             "image": "https://www.govtrack.us/data/photos/412556-200px.jpeg",
@@ -58,22 +58,30 @@ function getRecentStatusUpdates(callbackFn) {
     // as it would be with a real AJAX call.
     setTimeout(function() {
         callbackFn(SENATOR_LIST);
-    }, 1);
+    }, 100);
 }
 
 // this function stays the same when we connect
 // to real API later
-function displayStatusUpdates(data) {
+function displaySenators(data) {
     for (var index in data.senators) {
-        $('body').append(
-            '<p>' + data.senators[index].text + '</p>');
+        $('body').append("<img src="+ data.senators[index].image + '>');
+        $('body').append('<p>' + data.senators[index].firstname + ' ' + data.senators[index].lastname + '</p>');
+        $('body').append('<p> Rank: ' + data.senators[index].rank + '</p>');
+        $('body').append('<p> Party: ' + data.senators[index].party + '</p>');
+        $('body').append('<p> State: ' + data.senators[index].state + '</p>');
+        $('body').append('<p> Chairman/Ranking Member:' + data.senators[index].chairman_rankingmember + '</p>');
+        $('body').append('<p> Points:' + data.senators[index].points + '</p>');
+        if (data.senators[index].leadership_title) {
+        $('body').append('<p> Points:' + data.senators[index].points + '</p>');
+      }
     }
 }
 
 // this function can stay the same even when we
 // are connecting to real API
 function getAndDisplayStatusUpdates() {
-    getRecentStatusUpdates(displayStatusUpdates);
+    getRecentStatusUpdates(displaySenators);
 }
 
 //  on page load do this
